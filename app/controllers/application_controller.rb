360521665a0b153c51297a45cbf6afeecb5a0830
ADyusuf12 <adyusuf68@gmail.com>
2026-01-01 18:29:30 +0100

    Update redirect from root_path to home_index_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || root_path)
+    redirect_to(request.referrer || home_index_path)
   end
 
   def after_sign_in_path_for(resource)
