0e26b1377814e7397d1febbdd90f1a481417d35c
ADyusuf12 <adyusuf68@gmail.com>
2026-02-06 16:33:10 +0100

    split dotenvs and configure application.rb

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ActionController::Base
   http_basic_authenticate_with(
-    name: ENV.fetch("JOBS_USER"),
-    password: ENV.fetch("JOBS_PASSWORD")
+    name: ENV["JOBS_USER"],
+    password: ENV["JOBS_PASSWORD"]
   )
 end
