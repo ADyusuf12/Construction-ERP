71ab896ecdc7db13980ccf048d8dfb1ac5d96918
ADyusuf12 <adyusuf68@gmail.com>
2026-02-22 12:45:49 +0100

    Add logging for Mission Control Jobs auth

--- config/initializers/mission_control_jobs.rb
+++ config/initializers/mission_control_jobs.rb
@@ -1,11 +1,13 @@
+# config/initializers/mission_control_jobs.rb
 require "mission_control/jobs"
 
 Rails.application.configure do
-  # This order: Check ENV first (Render), then Credentials (Local), then Default
-  config.mission_control.jobs.authentication = {
-    user: ENV["JOBS_USER"] || Rails.application.credentials.dig(:mission_control, :jobs, :user) || "admin",
-    password: ENV["JOBS_PASSWORD"] || Rails.application.credentials.dig(:mission_control, :jobs, :password)
-  }
+  u = ENV["JOBS_USER"] || Rails.application.credentials.dig(:mission_control, :jobs, :user) || "admin"
+  p = ENV["JOBS_PASSWORD"] || Rails.application.credentials.dig(:mission_control, :jobs, :password)
 
+  # This will show up in your Render logs so you can see which one it picked
+  Rails.logger.info "MISSION CONTROL AUTH: User is #{u}, Password starts with #{p&.first(3)}..."
+
+  config.mission_control.jobs.authentication = { user: u, password: p }
   config.mission_control.jobs.adapters = [ :solid_queue ]
 end
